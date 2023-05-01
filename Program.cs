38837584be57eb77f8bad600dfbb608cf0bc1305
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-04-30 23:17:51 -0300

    atividade concluída

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("-- O maior entre os dois números --");
+Console.WriteLine("");
+Console.WriteLine("Digite um número para ser comparado...");
+
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro número para ser comparado...");
+
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 > numero2)
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine($"O número ({numero1}) é maior que o número ({numero2})!");
+}
+
+else 
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"O número ({numero1}) é menor que o número ({numero2})!");
+}
+Console.WriteLine("");
+Console.ResetColor();
